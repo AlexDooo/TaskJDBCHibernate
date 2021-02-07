68f8290371cb55da1873d8e948fc8bf2c6ca1e0b
AlexDooo <krash_pvp@mail.ru>
2021-02-07 18:09:23 +0300

    completed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Bob", "Trenton", (byte) 35);
+        userService.removeUserById(2);
+        userService.cleanUsersTable();
+        System.out.println(userService.getAllUsers().toString());
+        userService.dropUsersTable();
+
+
     }
 }
